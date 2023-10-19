94259fe833050dc872f71f449458565ebc8e9a15
Stanislav Červeňák <djsucho@gmail.com>
2023-10-19 09:10:59 +0200

    FIX names

---
+++ app/Nova/VatRate.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace App\Nova;
+
+use Laravel\Nova\Fields\BelongsTo;
+use Laravel\Nova\Fields\Number;
+use Laravel\Nova\Fields\Select;
+use ShuvroRoy\NovaTabs\Tab;
+use ShuvroRoy\NovaTabs\Tabs;
+use ShuvroRoy\NovaTabs\Traits\HasTabs;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Http\Requests\NovaRequest;
+use Wame\Utils\Helpers\Translator;
+
+class VatRate extends BaseResource
+{
+    use HasTabs;
+
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var string
+     */
+    public static $model = \App\Models\VatRate::class;
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = ['type', ' ', 'value', '%'];
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'id',
+    ];
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param NovaRequest $request
+     * @return array
+     */
+    public function fields(NovaRequest $request)
+    {
+        return [
+            Tabs::make(__('vat_rate.detail', ['title' => $this->title]), [
+                Tab::make(__('vat_rate.singular'), [
+                    ID::make()->onlyOnForms(),
+
+                    BelongsTo::make(__('vat_rate.field.country'), 'country', Country::class)
+                        ->help(__('vat_rate.field.country.help'))
+                        ->withSubtitles()
+                        ->withoutTrashed()
+                        ->showCreateRelationButton()
+                        ->sortable()
+                        ->filterable()
+                        ->required()
+                        ->rules('required')
+                        ->showOnPreview(),
+
+                    Select::make(__('vat_rate.field.type'), 'type')
+                        ->help(__('vat_rate.field.type.help'))
+                        ->options(Translator::arrayValue(config('wame-vat-rate.type')))
+                        ->displayUsing(fn () => __(config('wame-vat-rate.type.' . $this->type, $this->type)))
+                        ->searchable()
+                        ->sortable()
+                        ->filterable()
+                        ->required()
+                        ->rules('required')
+                        ->showOnPreview(),
+
+                    Number::make(__('vat_rate.field.value'), 'value')
+                        ->help(__('vat_rate.field.value.help'))
+                        ->displayUsing(fn () => $this->value . ' %')
+                        ->min(0)
+                        ->max(100)
+                        ->step(1)
+                        ->sortable()
+                        ->filterable()
+                        ->required()
+                        ->rules('required')
+                        ->showOnPreview(),
+                ]),
+            ])->withToolbar(),
+        ];
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param NovaRequest $request
+     * @return array
+     */
+    public function cards(NovaRequest $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param NovaRequest $request
+     * @return array
+     */
+    public function filters(NovaRequest $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param NovaRequest $request
+     * @return array
+     */
+    public function lenses(NovaRequest $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param NovaRequest $request
+     * @return array
+     */
+    public function actions(NovaRequest $request)
+    {
+        return [];
+    }
+
+}
--- app/Policies/VatRatePolicy.php
+++ app/Policies/VatRatePolicy.php
@@ -7,7 +7,7 @@ namespace App\Policies;
 use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
-class CountryPolicy
+class VatRatePolicy
 {
     use HandlesAuthorization;
 
