52c59e0293ff43b10d16c98fcdc987618af78f26
adrianzofcin <adrianzofcin@gmail.com>
2024-05-03 12:13:28 +0200

    Added en and cz translations

---
+++ resources/lang/cz/vat_rate.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'label' => 'DPH Sazby',
+    'plural' => 'DPH Sazby',
+    'singular' => 'DPH Sazba',
+    'detail' => 'DPH Sazba: :title',
+
+    'create.button' => 'Vytvořit DPH Sazbu',
+    'update.button' => 'Upravit DPH Sazbu',
+
+    'field.country' => 'Země',
+    'field.country.help' => 'Vyberte zemi',
+
+    'field.type' => 'Typ',
+    'field.type.help' => 'Vyberte typ DPH sazby',
+
+    'field.value' => 'DPH Sazba',
+    'field.value.help' => 'Zadejte DPH sazbu (%)',
+
+    'type.standard' => 'Standardní sazba',
+    'type.reduced' => 'Snížená sazba',
+    'type.super_reduced' => 'Velmi snížená sazba',
+    'type.parking' => 'Parkovací sazba',
+];
---
+++ resources/lang/en/vat_rate.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'label' => 'VAT Rates',
+    'plural' => 'VAT Rates',
+    'singular' => 'VAT Rate',
+    'detail' => 'VAT Rate: :title',
+
+    'create.button' => 'Create VAT Rate',
+    'update.button' => 'Edit VAT Rate',
+
+    'field.country' => 'Country',
+    'field.country.help' => 'Select country',
+
+    'field.type' => 'Type',
+    'field.type.help' => 'Select VAT rate type',
+
+    'field.value' => 'VAT Rate',
+    'field.value.help' => 'Enter VAT rate (%)',
+
+    'type.standard' => 'Standard Rate',
+    'type.reduced' => 'Reduced Rate',
+    'type.super_reduced' => 'Super Reduced Rate',
+    'type.parking' => 'Parking Rate',
+];
